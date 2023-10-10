8ee9927df3a90c47361f3cb034d62d096c5246db
teoma145 <matteodark145@yahoo.it>
2023-10-10 17:07:41 +0200

    tipologia-biglietti

--- javascrypt/javascrypt.js
+++ javascrypt/javascrypt.js
@@ -7,6 +7,7 @@ button.addEventListener('click', function(){
     let prezzobiglietto = (km * 0.21)
     let divnome = document.getElementById('namesur')
     let divcosto = document.getElementById('costo')
+    let divoffer = document.getElementById('offer')
     divnome.innerHTML =`<p> Nome: ${nome}`;
     if(nome == '' || km == '' || age == '')
     { alert('informazioni errate') }
@@ -14,5 +15,16 @@ button.addEventListener('click', function(){
         prezzobiglietto = prezzobiglietto - (prezzobiglietto * 20/100);
     prezzobiglietto = prezzobiglietto.toFixed(2);
     divcosto.innerHTML = prezzobiglietto
+    divoffer.innerHTML = ('Biglietto promo minorenni')
+    }
+    else if( age == "over"){
+        prezzobiglietto = prezzobiglietto - (prezzobiglietto * 40/100);
+    prezzobiglietto = prezzobiglietto.toFixed(2);
+    divcosto.innerHTML = prezzobiglietto
+    divoffer.innerHTML = ('Biglietto promo Anziani')
+    }
+    else{
+        divcosto.innerHTML = prezzobiglietto
+        divoffer.innerHTML = ('Biglietto Standard')
     }
 });
