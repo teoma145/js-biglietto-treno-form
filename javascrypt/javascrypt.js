836a0682406338df7666047791492510aa744866
teoma145 <matteodark145@yahoo.it>
2023-10-10 16:36:18 +0200

    eventlister-name

--- javascrypt/javascrypt.js
+++ javascrypt/javascrypt.js
@@ -4,7 +4,7 @@ button.addEventListener('click', function(){
     const nome = document.getElementById('nome').value;
     let km = document.getElementById('Km').value;
     let age = document.getElementById('age').value;
-    console.log(nome);
-    console.log(km)
-    console.log(age)
+    let prezzobiglietto = (km * 0.21)
+    let divnome = document.getElementById('namesur')
+    divnome.innerHTML =`<p> Nome: ${nome}`
 });
